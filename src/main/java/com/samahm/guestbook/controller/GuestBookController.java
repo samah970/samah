a95035a668ddafbb481b408c7ecdfc6686125db2
samah970 <samah.faissl@gmail.com>
2018-04-26 22:13:32 +0100

    Initial Commit

--- src/main/java/com/samahm/guestbook/controller/GuestBookController.java
+++ src/main/java/com/samahm/guestbook/controller/GuestBookController.java
@@ -6,59 +6,49 @@ import org.springframework.web.bind.annotation.*;
 import com.samahm.guestbook.service.GuestBookService;
 
 import java.util.List;
-<<<<<<< HEAD
+
+
 @RequestMapping ("/api")
-=======
-
-
->>>>>>> 0d6d3764ed105654bfba32ffe9772691d895e601
 @RestController
 public class GuestBookController {
-
 
     @Autowired
     private GuestBookService guestBookService;
 
-<<<<<<< HEAD
-    @GetMapping ("/comments")
-    public List <GuestBookEntry> getAllComments () {
-        return guestBookService.findAllEntries ();
-=======
-    @GetMapping("/comments")
-    public List <GuestBookEntry> testMapping() {
+    @GetMapping("/")
+    public List<GuestBookEntry> getAll() {
         return guestBookService.findAllEntries();
->>>>>>> 0d6d3764ed105654bfba32ffe9772691d895e601
     }
 
-    @GetMapping ("/comment/{id}")
-    public GuestBookEntry findGuestBookEntryById (@PathVariable("id") Integer id) {
-        return this.guestBookService.findGuestBookEntryById (id);
+
+    @GetMapping("/comments")
+    public List<GuestBookEntry> getAllComments() {
+        return guestBookService.findAllEntries();
     }
+
+
+    @GetMapping("/comment/{id}")
+    public GuestBookEntry findGuestBookEntryById(@PathVariable("id") Integer id) {
+        return this.guestBookService.findGuestBookEntryById(id);
+    }
+
     @DeleteMapping("/comment/{id}")
-    public void deleteGuestBookEntryById (@PathVariable ("id") Integer id) {
-        this.guestBookService.deleteGuestBookEntryById (id);
+    public void deleteGuestBookEntryById(@PathVariable("id") Integer id) {
+        this.guestBookService.deleteGuestBookEntryById(id);
     }
 
-    @GetMapping ("/user/{user}")
-    public List <GuestBookEntry> findGuestBookEntryByUser (@PathVariable ("user") String user) {
-        return this.guestBookService.findGuestBookEntryByUser (user);
+    @GetMapping("/user/{user}")
+    public List<GuestBookEntry> findGuestBookEntryByUser(@PathVariable("user") String user) {
+        return this.guestBookService.findGuestBookEntryByUser(user);
     }
-<<<<<<< HEAD
 
-    @PostMapping ("/add")
-=======
     @PostMapping("/add")
->>>>>>> 0d6d3764ed105654bfba32ffe9772691d895e601
-    public void addComment (@RequestBody GuestBookEntry guestBookEntry) {
-        this.guestBookService.save (guestBookEntry);
+    public void addComment(@RequestBody GuestBookEntry guestBookEntry) {
+        this.guestBookService.save(guestBookEntry);
     }
 
-    @PostMapping ("/update")
-    public void updateComment (@RequestBody GuestBookEntry guestBookEntry) {
-        this.guestBookService.save (guestBookEntry);
+    @PostMapping("/update")
+    public void updateComment(@RequestBody GuestBookEntry guestBookEntry) {
+        this.guestBookService.save(guestBookEntry);
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 0d6d3764ed105654bfba32ffe9772691d895e601
 }
